b21233cec205cb3b595e039828e3915006b5de00
CrispyChillies <pnhlong22@clc.fitus.edu.vn>
2023-12-16 08:41:56 +0700

    Fix filter function

--- client_filter.py
+++ client_filter.py
@@ -19,18 +19,20 @@ class email_filter:
         return [filter_rule(rule) for rule in config.get('Filter', [])]
 
     def classify_email(self, email: mailbox.mboxMessage):
-        sender = email['From'].lower()
-        subject = email['Subject'].lower()
+        sender = email.get('From', '').lower()
+        subject = email.get('Subject', '').lower()
         payload = email.get_payload()
 
-        if isinstance(payload, list):
+        if isinstance(payload, list) and payload:
             # If there are multiple parts, assume the first part is the text/plain part
             text_part = payload[0]
             content = text_part.get_payload().lower()
-        else:
+        elif isinstance(payload, str):
             # If there is only one part, treat it as the text/plain part
             content = payload.lower()
-
+        else:
+            content = ""
+            
         for filter_rule in self.config:
             if filter_rule.from_addresses and any(addr in sender for addr in filter_rule.from_addresses):
                 return filter_rule.to_folder
